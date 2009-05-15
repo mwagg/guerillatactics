02b29fdc58bfaf83cbf2eff59c98a7de681b5b1d
unknown <Michael@.(none)>
2009-05-15 11:57:12 +0100

    Added tools folder and nunit runner exe. Added nunit rake task. Modified guerillatactics build to run tests

--- GuerillaTactics/rakefile.rb
+++ GuerillaTactics/rakefile.rb
@@ -1,9 +1,13 @@
-$LOAD_PATH.push File.join(File.dirname(__FILE__), "..")
+rakefile_dir = File.dirname(__FILE__)
+
+$LOAD_PATH.push File.join(rakefile_dir, "..")
 
 require 'buildsupport/msbuild'
+require 'buildsupport/nunit'
 
-BUILD_OUTPUT_DIR = '../build'
-SOLUTION_FILE = File.join(File.dirname(__FILE__), 'guerillatactics.sln')
+NUNIT_CONSOLE_PATH = File.join(rakefile_dir, '../tools/nunit/nunit-console.exe')
+BUILD_OUTPUT_DIR = File.join(rakefile_dir, '../build')
+SOLUTION_FILE = File.join(rakefile_dir, 'guerillatactics.sln')
 
 task :clean do
 	if File.exists?(BUILD_OUTPUT_DIR)
@@ -11,8 +15,16 @@ task :clean do
 	end
 end
 
-Rake::MSBuildTask.new(:build_guerillatactics) do |msbuild|
+Rake::MSBuildTask.new(:build) do |msbuild|
 	msbuild.project_file = SOLUTION_FILE
+end
+
+Rake::NUnitTask.new(:test) do |nunit|
+	nunit.nunit_console_path = NUNIT_CONSOLE_PATH
+	
+	Dir.glob("**/bin/release/*Tests.dll").each do |test_assembly|
+		nunit.addTestAssembly(test_assembly)
+	end
 end
 
 task :copy_to_build do
@@ -22,4 +34,4 @@ task :copy_to_build do
 	end
 end
 
-task :default => [:clean, :build_guerillatactics, :copy_to_build]
+task :default => [:clean, :build, :test, :copy_to_build]
