a267591d3905da77610af48eb942ee6527767ac7
mwagg <michael@guerillatactics.co.uk>
2009-10-25 22:57:22 +0000

    Removed git configure scripts

--- DevConfigs/configure_git.cmd
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-git config --global user.name "mwagg"
-git config --global user.email michael@guerillatactics.co.uk
-
--- DevConfigs/configure_git.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-git config --global user.name "mwagg"
-git config --global user.email michael@guerillatactics.co.uk
